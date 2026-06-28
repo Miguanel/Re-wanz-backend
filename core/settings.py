03674590ed424ebd985e90bd4d938bca397abdc2
Migu <michauasota@gmail.com>
2026-06-28 15:14:19 +0200

    alloowed host

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-th0d$vg9z$0zrwy3t+=5gqloce@-ac=hki*w2_ovtda4ll*7x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
