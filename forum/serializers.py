727b242f62a8b7ef6a505b88117367de5381076d
Migu <michauasota@gmail.com>
2026-06-28 22:29:30 +0200

    debug true

--- forum/serializers.py
+++ forum/serializers.py
@@ -1,20 +1,15 @@
 from rest_framework import serializers
-from .models import ForumPost, ForumComment
+from .models import ForumPost
 from django.contrib.auth import get_user_model
+from users.serializers import SharedImageSerializer
 
 User = get_user_model()
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    """Zwraca tylko nazwę autora, żeby nie wysyłać całego konta z hasłami!"""
-
     class Meta:
         model = User
         fields = ['username', 'level']
-
-
-# Importujemy tłumacz zdjęć, aby API ładnie formatowało linki do obrazków
-from users.serializers import SharedImageSerializer
 
 
 class ForumPostSerializer(serializers.ModelSerializer):
@@ -22,28 +17,24 @@ class ForumPostSerializer(serializers.ModelSerializer):
     timeAgo = serializers.SerializerMethodField()
     images = SharedImageSerializer(many=True, read_only=True)
 
-    uploaded_image_ids = serializers.ListField(
-        child=serializers.IntegerField(), write_only=True, required=False
-    )
+    # 1. WŁAŚCIWY ODCZYT (GET) - Metoda gwarantująca brak błędu 500
+    tags = serializers.SerializerMethodField()
 
-    # 1. POLE DO ODCZYTU: Gdy Android robi GET, wysyłamy ładną listę stringów
-    tags = serializers.SlugRelatedField(
-        many=True, read_only=True, slug_field='name'
-    )
-
-    # 2. POLE DO ZAPISU: Zmieniamy nazwę na 'uploaded_tags', żeby nie gryzło się z oryginałem
+    # 2. WŁAŚCIWY ZAPIS (POST) - Zbieranie tagów z formularza Androida
     uploaded_tags = serializers.ListField(
         child=serializers.CharField(), write_only=True, required=False
     )
 
+    uploaded_image_ids = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=False
+    )
+
     class Meta:
         model = ForumPost
-        # Zamieniamy miejscami pola wejściowe
         fields = ['id', 'author', 'title', 'content', 'images', 'uploaded_image_ids', 'uploaded_tags', 'tags',
                   'upvotes', 'views', 'bounty', 'is_resolved', 'timeAgo']
 
     def create(self, validated_data):
-        # 3. Wyciągamy dane z odpowiednich pół "write_only"
         image_ids = validated_data.pop('uploaded_image_ids', [])
         tags_data = validated_data.pop('uploaded_tags', [])
 
@@ -53,10 +44,21 @@ class ForumPostSerializer(serializers.ModelSerializer):
             post.images.set(image_ids)
 
         if tags_data:
-            # Jeśli używasz django-taggit:
             post.tags.add(*tags_data)
 
         return post
 
     def get_timeAgo(self, obj):
         return obj.created_at.strftime("%d/%m/%Y, %H:%M")
+
+    # Bezpieczne wydobywanie tagów (jako zwykłe stringi)
+    def get_tags(self, obj):
+        # Sprawdzamy, czy model faktycznie ma pole 'tags', by uniknąć błędu AttributeError
+        if hasattr(obj, 'tags'):
+            # Funkcja names() działa, jeśli używasz django-taggit
+            if hasattr(obj.tags, 'names'):
+                return list(obj.tags.names())
+            # Jeśli używasz własnego modelu Tag(name=CharField())
+            elif hasattr(obj.tags, 'all'):
+                return [tag.name for tag in obj.tags.all()]
+        return []
