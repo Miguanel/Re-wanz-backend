28a7394caefd06b3351ed3aa9b390db1a1d14991
Migu <michauasota@gmail.com>
2026-06-28 16:04:11 +0200

    poprawka apitoken w urls settings

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <-- Czy tutaj na pewno jest 'api/'?
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     # Endpointy dla Twoich aplikacji w Androidzie (Zwróć uwagę na przedrostek 'api/')
     path('api/forum/', include('forum.urls')),
