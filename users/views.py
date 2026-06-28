bdaadc954cb40cde8e5d17c7628bde0595f48f59
Migu <michauasota@gmail.com>
2026-06-28 15:52:08 +0200

    Dodanie logiki rejestracji i nadanie publicznego dostepu

--- users/views.py
+++ users/views.py
@@ -1,23 +1,54 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import UserProfileSerializer
 
-
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API dla profili użytkowników.
-    Używamy ReadOnlyModelViewSet, aby zablokować ręczne modyfikacje profilu z zewnątrz na tym etapie.
-    GET /api/users/ -> Lista wszystkich wilków (np. do rankingu)
     """
     queryset = CustomUser.objects.all()
     serializer_class = UserProfileSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    # AKTUALIZACJA: Dynamiczne uprawnienia
+    def get_permissions(self):
+        # Jeśli to zapytanie o rejestrację, pozwól wejść każdemu (AllowAny)
+        if self.action == 'register':
+            return [permissions.AllowAny()]
+        # W każdym innym przypadku (np. oglądanie profili), wymagaj logowania
+        return [permissions.IsAuthenticated()]
 
     # Niestandardowy Endpoint "Mój Profil"
     @action(detail=False, methods=['get'])
     def me(self, request):
-        """ GET /api/users/me/ -> Pobiera profil osoby aktualnie zalogowanej (na podstawie tokena JWT) """
+        """ GET /api/users/me/ """
         serializer = self.get_serializer(request.user)
         return Response(serializer.data)
+
+    # NOWOŚĆ: Endpoint Rejestracji
+    @action(detail=False, methods=['post'])
+    def register(self, request):
+        """ POST /api/users/register/ """
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+
+        if not username or not password or not email:
+            return Response({'error': 'Wypełnij wszystkie pola'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Sprawdzamy, czy nazwa nie jest już zajęta
+        if CustomUser.objects.filter(username=username).exists():
+            return Response({'error': 'Taki wilk już istnieje. Wybierz inną nazwę.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Tworzymy nowe konto w bazie
+        user = CustomUser.objects.create(
+            username=username,
+            email=email
+        )
+        # BARDZO WAŻNE: Funkcja set_password szyfruje hasło.
+        # Bez niej hasło byłoby jawnym tekstem i logowanie JWT nie zadziała!
+        user.set_password(password)
+        user.save()
+
+        return Response({'message': 'Konto utworzone pomyślnie!'}, status=status.HTTP_201_CREATED)
