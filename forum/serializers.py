f240fb9f9cb5c6b53dd7be5537114328acda36bd
Migu <michauasota@gmail.com>
2026-06-29 15:16:25 +0200

    pupdate serailized meta dane images

--- forum/serializers.py
+++ forum/serializers.py
@@ -1,37 +1,43 @@
 from rest_framework import serializers
 from .models import ForumPost
 from django.contrib.auth import get_user_model
-from users.serializers import SharedImageSerializer
+# Import niepotrzebny, jeśli go nie używasz wewnątrz klasy
+# from users.serializers import SharedImageSerializer
 
 User = get_user_model()
-
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'level']
 
-
 class ForumPostSerializer(serializers.ModelSerializer):
     author = AuthorSerializer(read_only=True)
     timeAgo = serializers.SerializerMethodField()
-
-    # 1. WŁAŚCIWY ODCZYT (GET) - Metoda gwarantująca brak błędu 500
     tags = serializers.SerializerMethodField()
 
-    # 2. WŁAŚCIWY ZAPIS (POST) - Zbieranie tagów z formularza Androida
-    uploaded_tags = serializers.ListField(
-        child=serializers.CharField(), write_only=True, required=False
+    # Zmiana: zamieniamy 'post_images' na 'images' (zgodnie z tym co masz w metodzie create)
+    # Jeśli chcesz, żeby w JSONie z API pole nazywało się 'post_images',
+    # użyj source='images'
+    post_images = serializers.PrimaryKeyRelatedField(
+        source='images', many=True, read_only=True
     )
 
     uploaded_image_ids = serializers.ListField(
         child=serializers.IntegerField(), write_only=True, required=False
     )
+    uploaded_tags = serializers.ListField(
+        child=serializers.CharField(), write_only=True, required=False
+    )
 
     class Meta:
         model = ForumPost
-        fields = ['id', 'author', 'title', 'content', 'post_images', 'uploaded_image_ids', 'uploaded_tags', 'tags',
-                  'upvotes', 'views', 'bounty', 'is_resolved', 'timeAgo']
+        # Używamy poprawnej nazwy pola 'post_images' zdefiniowanej wyżej
+        fields = [
+            'id', 'author', 'title', 'content', 'post_images',
+            'uploaded_image_ids', 'uploaded_tags', 'tags',
+            'upvotes', 'views', 'bounty', 'is_resolved', 'timeAgo'
+        ]
 
     def create(self, validated_data):
         image_ids = validated_data.pop('uploaded_image_ids', [])
@@ -40,9 +46,11 @@ class ForumPostSerializer(serializers.ModelSerializer):
         post = ForumPost.objects.create(**validated_data)
 
         if image_ids:
+            # Upewnij się, że .images to ManyToManyField w modelu ForumPost
             post.images.set(image_ids)
 
         if tags_data:
+            # Upewnij się, że .tags to TaggableManager (django-taggit)
             post.tags.add(*tags_data)
 
         return post
@@ -50,14 +58,10 @@ class ForumPostSerializer(serializers.ModelSerializer):
     def get_timeAgo(self, obj):
         return obj.created_at.strftime("%d/%m/%Y, %H:%M")
 
-    # Bezpieczne wydobywanie tagów (jako zwykłe stringi)
     def get_tags(self, obj):
-        # Sprawdzamy, czy model faktycznie ma pole 'tags', by uniknąć błędu AttributeError
+        # Sprawdzenie, czy pole istnieje, żeby nie wywalało błędu
         if hasattr(obj, 'tags'):
-            # Funkcja names() działa, jeśli używasz django-taggit
+            # Dla django-taggit:
             if hasattr(obj.tags, 'names'):
                 return list(obj.tags.names())
-            # Jeśli używasz własnego modelu Tag(name=CharField())
-            elif hasattr(obj.tags, 'all'):
-                return [tag.name for tag in obj.tags.all()]
         return []
